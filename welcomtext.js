25891ed2b51a5e8552d02ac9a3c2f8c1d17314eb
KingHackYT <80841806+KingHackYT@users.noreply.github.com>
2021-08-14 21:02:18 -0300

    Update welcomtext.js

--- welcomtext.js
+++ welcomtext.js
@@ -1,9 +1,9 @@
 const welcome = (number, groupname) => {
-    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+    return `Hola @${number}. Bienvenid@ al grupo ${groupname}`
 }
 exports.welcome = welcome
 
 const bye = (number) => {
-    return `Adeus @${number}. Já vai tarde 👋`
+    return `Chau @${number}. Hasta luego 👋`
 }
 exports.bye = bye
